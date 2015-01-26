a4d2c823f9eea38ec36ebe6140fe576dbf13eab0
Nabid <mail.prome@gmail.com>
2015-01-26 22:06:27 +0600

    initial commit

---
+++ install_bangla_fonts.sh
@@ -0,0 +1,1 @@
+wget https://github.com/Nabid/Install_Bangla_Fonts/archive/master.zip -O Install_Bangla_Fonts.zip && unzip -x Install_Bangla_Fonts.zip && cd Install_Bangla_Fonts-master/ && chmod +x bangla_fonts.sh && ./bangla_fonts.sh  && cd .. && rm -r Install_Bangla_Fonts-master/ && echo 'Above bangla fonts has been successfully installed.' 
